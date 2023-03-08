bdf3e66092faf14cee693b1a3f0517e63afcbf73
“Yoo” <12hongkildong@gmail.com>
2023-03-08 12:35:20 +0900

    Docs(setting): update html

--- res/js/setting.js
+++ res/js/setting.js
@@ -1,11 +1,12 @@
 
 // 프로필-------------------------------------------
+
 window.addEventListener("load",function(){
     let s1 = document.querySelector("section");
     let profileBtn = s1.querySelector(".upload-btn");
     let profileTrigerBtn = s1.querySelector(".pro-img");
 
-    profileTrigerBtn.onclick = function(){
+    profileTrigerBtn.onclick = function(){       // 프로필 이미지를 누르면 input type="file"이 실행되는 트리거
         let event = new MouseEvent("click", {
             'view':window,
             'bubbles': true,
@@ -14,10 +15,13 @@ window.addEventListener("load",function(){
         profileBtn.dispatchEvent(event)
     }
 
-    profileBtn.addEventListener("change",()=>{
+    profileBtn.addEventListener("change",()=>{  // 사진을 올리면 프로필 사진이 바뀌게 만들기
         let reader = new FileReader();
         reader.readAsDataURL(profileBtn.files[0]);
-        reader
+        console.log(profileBtn.files[0]);
+        profileTrigerBtn.style.backgroundImage=url(profileBtn.files[0]);
+            
+    
     })
 
 });
