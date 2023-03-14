6b8e15df390e595bbaec92fca0d583fa6823804e
SeonRilla <SeonRilla97@gmail.com>
2023-03-14 11:34:25 +0900

    Feat(singup): add slider.js convert es5 to es6

--- res/js/mainLetter.js
+++ res/js/mainLetter.js
@@ -1,5 +1,5 @@
 const HITEXT = ["안녕하세요!", "하루를 정리해주세요","오길 기다렸어요", "이야기를 들려주세요","어서오고","반갑고"]
 
 let addHello = document.querySelector("#add-hello");
-console.log(Math.floor(Math.random() * HITEXT.length))
+// console.log(Math.floor(Math.random() * HITEXT.length))
 addHello.insertAdjacentHTML('beforeend',`<div class="h2 mgt-2">${HITEXT[Math.floor(Math.random() * HITEXT.length)]}`)
