35c5a1734bd1562f5402b35cbaeaaf66cdf08b60
LDH <wuzget0508@naver.com>
2023-03-20 18:10:36 +0900

    Design(index): 집에서 마무리 하기 전에 커밋해두기

--- res/js/index.js
+++ res/js/index.js
@@ -1,6 +1,6 @@
 //자바스크립트로 이벤트 주기위한 변수들 설정
 let words=document.querySelectorAll(".index-text-fade");
-let sections = document.querySelectorAll(".index-section")
+let sections = document.querySelectorAll(".index-div")
 let currentNum =0;
 let beforeNum =0;
 let wordArray = [];
@@ -26,7 +26,7 @@ let observer = new IntersectionObserver((e)=>{
 
 for(let  i=0; i<sections.length; i++){
         observer.observe(sections[i]);
-     
+
 }
 
 
@@ -46,5 +46,53 @@ fadeFunction=setInterval(function(){
     words[beforeNum].classList.remove("fade-in");
     words[beforeNum].classList.add("d-none");
 
-  
+
 },3000);
+
+
+////////////스크롤 이벤트/////////////////////
+let html = document.querySelector("html");
+var page = 1;
+var lastPage = document.querySelectorAll(".content");
+
+window.addEventListener('wheel', function(e) {
+    // 만약 html이 애니메이션 중인 경우 함수 실행 중지
+    if (html.classList.contains('is-animated')) return;
+    
+    // 마우스 휠의 deltaY 값에 따라 page 변수 값 변경
+    if (e.deltaY > 0) {
+        if (page === lastPage) return;
+        page++;
+    } else if (e.deltaY < 0) {
+        if (page === 1) return;
+        page--;
+    }
+    
+    // 새롭게 계산된 위치를 기준으로 scrollTop 애니메이션 실행
+    var posTop = (page-1) * window.innerHeight;
+    html.classList.add('is-animated');
+    scrollTo(html, posTop, 500, function() {
+    html.classList.remove('is-animated');
+    });
+});
+
+
+
+  // 스크롤 애니메이션 함수
+function scrollTo(element, to, duration, callback) {
+    if (duration <= 0) {
+        callback();
+        return;
+    }
+    var difference = to - element.scrollTop;
+    var perTick = difference / duration * 10;
+    
+    setTimeout(function() {
+        element.scrollTop = element.scrollTop + perTick;
+        if (element.scrollTop === to) {
+        callback();
+        return;
+        }
+    scrollTo(element, to, duration - 10, callback);
+    }, 10);
+}
