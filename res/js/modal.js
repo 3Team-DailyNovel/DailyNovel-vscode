3220ab21c7c3b846af20535ee8e00eee41167aab
DongDong107 <jkds989@gmail.com>
2023-03-27 21:47:53 +0900

    Design(register) : 애니메이션 및 모달 추가 중이나 DOM 2회 이상 적용안되는  오류 고쳐야함

--- res/js/modal.js
+++ res/js/modal.js
@@ -1,7 +1,7 @@
 window.addEventListener('load',()=>{
   const tmpSelectBox = document.querySelector(".tmp-select-box");   
   const simpleTmp = document.querySelector("#simple-tmp");
-  const container = document.querySelector(".container-2");
+  let container = document.querySelector(".container-2");
 
   const tmpBox = document.querySelector("#tmp-value");
   const feelBox = document.querySelector("#feel-value");
@@ -9,34 +9,51 @@ window.addEventListener('load',()=>{
 
   // 모달 관련
   const regTmpModal = document.querySelector("#reg-tmp-modal");
-  const modalCloseBtn = document.querySelector("#modal-close-btn");
-  const modal = document.querySelector(".reg-select-modal");
+  const regFeelModal = document.querySelector("#reg-feel-modal");
+  const modalCloseBtn = document.querySelectorAll("#modal-close-btn");
+  const regmodal = document.querySelector(".reg-select-modal");
 
   tmpBox.addEventListener('click', ()=> {
     modalOpen(regTmpModal,container);
   })   
 
-  modalCloseBtn.addEventListener('click', ()=>{
-    modalClose(modal,container);
+  feelBox.addEventListener('click', ()=>{
+    modalOpen(regFeelModal,container);
   })
 
+  for(let e of modalCloseBtn){
+    e.addEventListener('click', ()=> {
+      let activeModal = document.querySelector(".reg-select-modal.active");
+      activeModal.addEventListener('click', ()=> {
+        console.log("ghkrls");
+      })
+      modalClose(activeModal,container);
+      modalClose(regmodal,container);  
+    })
+  }
+/*
+  modalCloseBtn.addEventListener('click', ()=>{
+    let activeModal = document.querySelector(".reg-select-modal.active");
+    activeModal.addEventListener('click', ()=> {
+      console.log("ghkrls");
+    })
+    modalClose(activeModal,container);
+    modalClose(regmodal,container);
+  })
+*/
   function modalOpen(modal, container){
-    modal.classList.add('modal-active');
-    modal.classList.toggle('pointer-none');
-    container.classList.toggle('pointer-none');
+    modal.classList.add('active');        
     container.classList.toggle('position-fixed');
   }
 
   function modalClose(modal, container){
-    modal.classList.remove('modal-active');
-    modal.classList.toggle('pointer-none');
-    container.classList.toggle('pointer-none');
+    modal.classList.remove('active');      
     container.classList.toggle('position-fixed');
   }
 
   // 추후에 드랍다운 js 만들면서 빠져야할 부분.
   simpleTmp.addEventListener('click', ()=>{
-    tmpSelectBox.classList.toggle("d-none");
+    // tmpSelectBox.classList.toggle("d-none");
     tmpSelectBox.classList.toggle("active");
     simpleTmp.classList.toggle("color-bg-green-2");
     
