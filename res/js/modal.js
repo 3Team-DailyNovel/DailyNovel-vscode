02693b7bb7271042b8ac7345a846d850822519f7
DongDong107 <jkds989@gmail.com>
2023-03-29 18:12:45 +0900

    Design(register) : 작성 창 모달들 작업 중

--- res/js/modal.js
+++ res/js/modal.js
@@ -6,6 +6,8 @@ window.addEventListener('load',()=>{
   const tmpBox = document.querySelector("#tmp-value");
   const feelBox = document.querySelector("#feel-value");
   const weatherBox = document.querySelector("#weather-value");
+  const regCancelBtn = document.querySelector("#reg-cancel-btn");
+  const regSumbitBtn = document.querySelector("#reg-submit-btn");
 
   const simpleTmpBox = document.querySelectorAll(".tmp-select-box > *");
 
@@ -15,6 +17,16 @@ window.addEventListener('load',()=>{
   const regWeatherModal = document.querySelector("#reg-weather-modal");
   const modalCloseBtn = document.querySelectorAll("#modal-close-btn");
   const regmodal = document.querySelector(".reg-select-modal");
+  const regCancelModal = document.querySelector("#reg-cancel-modal");
+  const regHonestyModal = document.querySelector("#reg-honesty-select-modal");
+
+  regSumbitBtn.addEventListener('click', ()=>{
+    modalOpen(regHonestyModal,container);
+  })
+
+  regCancelBtn.addEventListener('click', ()=>{
+    modalOpen(regCancelModal,container);
+  })
 
   tmpBox.addEventListener('click', ()=> {
     modalOpen(regTmpModal,container);
@@ -27,6 +39,8 @@ window.addEventListener('load',()=>{
   weatherBox.addEventListener('click', ()=>{
     modalOpen(regWeatherModal,container);
   })
+
+  
 
   for(let e of modalCloseBtn){
     e.addEventListener('click', ()=> {
